23a82aa29ec9b1dc17d42fa833ae3f32d484c0c8
neoan <stefan.roehrl@gmail.com>
2024-10-28 15:53:50 -0400

    Step 2: Struktur vereinfachen. "Resets" in die submit Funktion packen. Natives Formular benutzen.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,19 @@
     let errorMessage = '';
 
     async function fetchAge() {
-        if (!name.trim()) {
+        // resets hier
+        ageData.age = null;
+        errorMessage = '';
+
+        // nicht mehr nötig. "require" wirds lösen
+        /*if (!name.trim()) {
             errorMessage = 'Name cannot be empty.';
             return;
-        }
+        }*/
 
         loading = true;
         errorMessage = '';
         try {
-
             ageData = await get(`?name=${name}`)
         } catch (error) {
             console.error(error.message);
@@ -27,25 +31,20 @@
         }
     }
 
-    function handleInput(event) {
-        name = event.target.value;
-        ageData.age = null;
-        errorMessage = '';
-    }
 </script>
 
 <main class="container mx-auto p-4 text-center">
     <h1 class="mb-8 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Predict Your Age with AI.</h1>
-    <div class="flex space-x-4 justify-center">
-        <Input 
-            class="w-full sm:w-1/2" 
-            bind:value={name} 
-            on:input={handleInput} 
-            placeholder="Enter your name" 
-            on:keypress={(e) => e.key === 'Enter' && fetchAge()} 
+    <form class="flex space-x-4 justify-center" on:submit|preventDefault={fetchAge}>
+        <Input
+                required
+                class="w-full sm:w-1/2"
+                bind:value={name}
+                placeholder="Enter your name"
         />
-        <Button on:click={fetchAge}>Predict Age</Button>
-    </div>
+        <Button type="submit">Predict Age</Button>
+    </form>
+
 
     {#if loading && ageData.age !== null}
         <p class="text-lg font-medium text-gray-700 dark:text-gray-300 mt-4">Loading...</p>
